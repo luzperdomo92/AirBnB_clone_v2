574aca7288da8a0f5ce80987043fc118da5a665b
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-17 21:03:24 -0500

    Added do_deploy fabric cmd

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+from fabric.api import local, env, run
+from datetime import datetime
+import os
+
+env.hosts = [
+    '35.243.241.181',
+    '3.87.74.240'
+]
+
+
+def do_pack():
+    """packs the content of web_static"""
+
+    folder_name = "web_static"
+    now = datetime.now()
+    today_str = now.strftime("%Y%m%d%H%M%S")
+    file_name = "%s_%s.tgz" % (folder_name, today_str)
+    file_path = "versions/%s" % (file_name)
+
+    result = local("mkdir -p versions")
+    if result.failed:
+        return None
+    result = local("tar -cvzf %s %s" % (file_path, folder_name))
+    if result.failed:
+        return None
+    return file_name
+
+
+def do_deploy(archive_path):
+    """ Do Deploy """
+    # check the file if exits
+    if not os.path.exist(archive_path):
+        return False
+
+    # web_static_20170315003959.tgz
+    file_name = os.path.basename(archive_path)
+    # web_static_20170315003959
+    file_name_wo_ext = os.path.splitext(file_name)[0]
+
+    put(archive_path, '/tmp/%s'.format(file_name))
+    run("mkdir -p /data/web_static/releases/%s".format(file_name_wo_ext))
+
+    run("tar -xzf /tmp/%s -C /data/web_static/releases/%s/".format(
+        file_name, file_name_wo_ext))
+    
+    run("rm /tmp/%s".format(file_name))
+    run("mv /data/web_static/releases/%s/web_static/* \
+        /data/web_static/releases/%s/".format(file_name_wo_ext,
+                                              file_name_wo_ext))
+    run("rm -rf /data/web_static/releases/%s/web_static".format(
+        file_name_wo_ext))
+
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/%s/ /data/web_static/current".format(
+        file_name_wo_ext))
