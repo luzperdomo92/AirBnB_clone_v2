7aecf3f634d2b85f8ddaae83c8f479fdddb054aa
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-17 20:02:07 -0500

    fixed return data in do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,12 @@ def do_pack():
     today = date.today()
     today_str = today.strftime("%Y%m%d%l%M%S")
     file_name = "%s_%s.tgz" % (folder_name, today_str)
+    file_path = "versions/%s" % (file_name)
 
-    local("mkdir -p versions")
-    local("tar -cvzf versions/%s %s" % (file_name, folder_name))
-    return file_name
+    result = local("mkdir -p versions")
+    if result.failed:
+        return None
+    result = local("tar -cvzf %s %s" % (archive_path, folder_name))
+    if result.failed:
+        return None
+    return file_path
