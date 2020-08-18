4e16ca9cc4fad04d4b9d534b4e5850aba30e2792
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-17 22:08:13 -0500

    removed do_pack from  2-do_deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,25 +9,6 @@ env.hosts = [
     '3.87.74.240'
 ]
 
-
-def do_pack():
-    """packs the content of web_static"""
-
-    folder_name = "web_static"
-    now = datetime.now()
-    today_str = now.strftime("%Y%m%d%H%M%S")
-    file_name = "%s_%s.tgz" % (folder_name, today_str)
-    file_path = "versions/%s" % (file_name)
-
-    result = local("mkdir -p versions")
-    if result.failed:
-        return None
-    result = local("tar -cvzf %s %s" % (file_path, folder_name))
-    if result.failed:
-        return None
-    return file_name
-
-
 def do_deploy(archive_path):
     """ Do Deploy """
     # check the file if exits
