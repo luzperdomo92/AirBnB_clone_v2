0444a78b2157a94d1e86887e33fdece65c00f0d7
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-17 22:28:47 -0500

    slashes added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,13 +24,13 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, '/tmp')
-        run("mkdir -p {}".format(release_path))
+        run("mkdir -p {}/".format(release_path))
         run("tar -xzf /tmp/{} -C {}/".format(file_name, release_path))
         run("rm /tmp/{}".format(file_name))
         run("mv {}/web_static/* {}/".format(release_path, release_path))
         run("rm -rf {}/web_static".format(release_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_path))
+        run("ln -s {}/ /data/web_static/current".format(release_path))
         return True
     except:
         return False
