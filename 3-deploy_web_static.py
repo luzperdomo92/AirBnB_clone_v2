baa4cf5b84345a73f61e33b5bfd9f64a5eae0650
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-17 22:53:55 -0500

    fixed error handle in def do_pack():

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -27,14 +27,12 @@ def do_pack():
     today_str = now.strftime("%Y%m%d%H%M%S")
     file_name = "%s_%s.tgz" % (folder_name, today_str)
     file_path = "versions/%s" % (file_name)
-
-    result = local("mkdir -p versions")
-    if result.failed:
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf %s %s" % (file_path, folder_name))
+        return file_name
+    except:
         return None
-    result = local("tar -cvzf %s %s" % (file_path, folder_name))
-    if result.failed:
-        return None
-    return file_name
 
 
 def do_deploy(archive_path):
