26525e1a81759f7bda179302450cfc32f4645ba8
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-31 18:42:42 -0500

    added task 1 and 0 with permissions

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 """  starts a Flask web application """
 from flask import Flask
+app = Flask(__name__)
 
-app = 
+@app.route('/')
+def hello_HBTN():
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def HBNB():
+	return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
