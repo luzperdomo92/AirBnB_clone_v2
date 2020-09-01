2e5f9f281cba5bc70dcca86bf14fcc79338ef192
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-01 11:12:49 -0500

    added task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -17,5 +17,15 @@ def C(text):
 	new_text = text.replace("_", " ")
 	return "C %s" % new_text
 
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text="is cool"):
+	new_text = text.replace("_", " ")
+	return "Python %s" % new_text
+
+@app.route('/number/<int:n>')
+def n_number(n):
+	return "%d is a number" % n
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
